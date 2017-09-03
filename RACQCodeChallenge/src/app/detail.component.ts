9554eea8b41e75d7881f9ea0db448664d609e972
Nate Watson <you@example.com>
2017-09-03 22:57:59 +1000

    Add call to fetch answers from data service: - wire up variables required for template - get selected question

--- RACQCodeChallenge/src/app/detail.component.ts
+++ RACQCodeChallenge/src/app/detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {DataService} from '../app/services/data.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
     selector: 'question-detail',
@@ -9,7 +10,26 @@ import {DataService} from '../app/services/data.service';
 
 export class DetailComponent {
 
-    ngOnInit() {
+    question: any;
+    questionAnswerThread: any = [];
+    answers: number = 0;
 
+    constructor(private _dataService: DataService, private _route: ActivatedRoute, private _router: Router) {
+        this.question = _dataService.chosenQuestion;
+     }
+
+    ngOnInit() {
+        this._dataService.getQuestionAnswers(this.question.question_id)
+            .subscribe (
+                data => {
+                    this.questionAnswerThread = data.items;
+                },
+                err => {
+                    // handle error TODO
+                },
+                () => {
+                    this.answers = Object.keys(this.questionAnswerThread).length;
+                }
+            )
     }
 }
