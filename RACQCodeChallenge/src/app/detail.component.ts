13b3190b75c88c77e8d4026af177398ef9266d6c
Nate Watson <you@example.com>
2017-09-03 16:20:04 +1000

    Create Questions Detail Component and Template - test click on question in master list and check that navigation has occurred to DetailComponent - OK

---
+++ RACQCodeChallenge/src/app/detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import {DataService} from '../app/services/data.service';
+
+@Component({
+    selector: 'question-detail',
+    templateUrl: './detail.component.html',
+    styleUrls: ['./app.component.css']
+})
+
+export class DetailComponent {
+
+    ngOnInit() {
+
+    }
+}
