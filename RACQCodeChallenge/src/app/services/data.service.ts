e88f025126483ffd21db63aebea253ce0b83c803
Nate Watson <you@example.com>
2017-09-03 22:52:33 +1000

    Add external calls to data service: - add function to get all answers for a question - add function to get all comments for an answer (needs more work)

--- RACQCodeChallenge/src/app/services/data.service.ts
+++ RACQCodeChallenge/src/app/services/data.service.ts
@@ -7,24 +7,49 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 
 const SO_PREFIX_URL:string = 'http://api.stackexchange.com/';
+const KEY:string = "&key=Nj6GQQcgNBY3w1InUR)Vgg((";
 
 @Injectable()
 export class DataService {
 
+    todaysQuestions: any[];
+    chosenQuestion: any = [];
+
     constructor (private _http: Http) {
+        this.getTodaysQuestions("ios");
+     }
 
-    }
-
-    getTodaysQuestions(tag:string):Observable<any> {
-        let url = SO_PREFIX_URL + "2.2/questions?fromdate=1504310400&order=desc&sort=activity&tagged=" + tag + "&site=stackoverflow";
-
-        return this._http.get(url).map((res:Response)=>res.json());
+    getTodaysQuestions(tag:string) {
+        let url = SO_PREFIX_URL + "2.2/questions?fromdate=1504396800&order=desc&sort=activity&tagged=" + tag + "&site=stackoverflow&filter=withbody" + KEY;
+        
+        return this._http.get(url).map((res:Response)=>res.json())
+                 /**  .subscribe(
+                       data => {
+                            this.todaysQuestions = data.items;
+                       },
+                       err => {
+                           // handle error TODO
+                       }
+                    
+                   )**/
     }
 
     getQuestionComments(questionIds: string):Observable<any> {
-        let url = SO_PREFIX_URL + "2.2/questions/" + questionIds + "/comments?order=desc&sort=creation&site=stackoverflow&filter=withbody";
+        let url = SO_PREFIX_URL + "2.2/questions/" + questionIds + "/comments?order=desc&sort=creation&site=stackoverflow&filter=withbody" + KEY;
 
         return this._http.get(url).map((res:Response)=>res.json());
+    }
+
+    getQuestionAnswers(questionID):Observable<any> {
+        let url = SO_PREFIX_URL + "2.2/questions/" + questionID + "/answers?order=desc&sort=activity&site=stackoverflow&filter=withbody" + KEY;
+
+        return this._http.get(url).map((res:Response)=>res.json());
+    }
+
+    getAnswerComments(answerID):Observable<any> {
+        let url = SO_PREFIX_URL;
+
+        return this._http.get(url).map((res:Response) => res.json());
     }
 
 }
