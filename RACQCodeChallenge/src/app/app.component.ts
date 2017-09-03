145f768266af3e37194f7f6002d9057ca0e4d278
Nate Watson <you@example.com>
2017-09-03 22:54:22 +1000

    Remove '/' from router-outlet template

--- RACQCodeChallenge/src/app/app.component.ts
+++ RACQCodeChallenge/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   template: `
-    <router-outlet>/<router-outlet>
+    <router-outlet><router-outlet>
   `,
 })
 export class AppComponent {
